13abe8de6fd1f70cb22f66bdecd63ee309de1cea
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2017-03-24 21:07:39 -0700

    didn't commit, cuz forgot to add, but here

--- psynth/script.js
+++ psynth/script.js
@@ -1,3 +1,37 @@
-/**
- * Created by Sumi.Govindaraju20 on 3/24/2017.
- */
+function templateToRNA() {
+    var template = document.getElementById('template').value;
+    var mRNA = '';
+    var dna = '';
+    var isValid = true;
+    for (var i = 0; i < template.length; i++) {
+        if (template.charAt(i).toLowerCase() == 'a') {
+            mRNA += 'U';
+            dna += 'T';
+        }
+        else if (template.charAt(i).toLowerCase() == 't') {
+            mRNA += 'A';
+            dna += 'A'
+        }
+        else if (template.charAt(i).toLowerCase() == 'g') {
+            mRNA += 'C';
+            dna += 'C';
+        }
+        else if (template.charAt(i).toLowerCase() == 'c') {
+            mRNA += 'G';
+            dna += 'G';
+        }
+        else if (template.charAt(i) == ' ') {
+            mRNA += ' ';
+            dna += ' ';
+        }
+        else {
+            isValid = false;
+            alert("Invalid Template DNA");
+            break;
+        }
+    }
+    if (isValid) {
+        document.getElementById('mRNA').value = mRNA;
+        document.getElementById('dna').value = dna;
+    }
+}
