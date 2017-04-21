436a2bcd3f70599707caf4e7d373e9b66df96d05
Sumukh Govindaraju <sumi.govindaraju20@bcp.org>
2017-04-20 21:15:36 -0700

    stuff

--- punnet/script.js
+++ punnet/script.js
@@ -21,37 +21,9 @@ function init() {
 }
 
 function getGametes() {
-    // for (var i = 0; i < gam1Arr.length; i++) {
-    //     gam1Arr[i] = 'aBcDe';
-    //     gam2Arr[i] = 'AbCdE';
-    // }
-    gam1Arr[0] = 'A';
-    gam1Arr[1] = 'a';
-    gam2Arr[0] = 'A';
-    gam2Arr[1] = 'a';
+    console.log("Getting Gametes");
 }
 
 function tableCreate() {
-    body = document.getElementsByTagName('body')[0];
-    tbl = document.createElement('table');
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
-    tbdy = document.createElement('tbody');
-    // for (var i = 0; i < gam2Arr.length; i++) {
-    //     var tr = document.createElement('tr');
-    //     for (var j = 0; j < gam1Arr.length; j++) {
-    //         var td = document.createElement('td');
-    //         var tableElement = "";
-    //         for (var k = 0; k < gam1Arr[i].length; k++) {
-    //             tableElement += gam1Arr[i].charAt(k) + gam2Arr[i].charAt(k);
-    //         }
-    //         td.appendChild(document.createTextNode(tableElement));
-    //         tr.appendChild(td);
-    //     }
-    //     tbdy.appendChild(tr);
-    // }
-    var tr = document.createElement('tr');
-
-    tbl.appendChild(tbdy);
-    body.appendChild(tbl);
+    console.log("Creating Table");
 }
