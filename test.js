6fa946cbed97d42e55ef1ce7bd096ec79f02d7ea
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2017-08-01 14:01:54 -0700

    Starting Demo Using default styling

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-// (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) 
-//         return;
-//     js = d.createElement(s); 
-//     js.id = id;
-//     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
-//     fjs.parentNode.insertBefore(js, fjs);
-// } (document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) 
+        return;
+    js = d.createElement(s); 
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+    fjs.parentNode.insertBefore(js, fjs);
+} (document, 'script', 'facebook-jssdk'));
 
 function loadJSON(url) {
     console.log('starting');
@@ -22,8 +22,7 @@ function loadJSON(url) {
         console.log('xmlhttp state changed');
         if (xmlhttp.readyState == 4) {
             if (xmlhttp.status == 200) {
-                document.getElementById("myBody").innerHTML = xmlhttp.responseText;
-                document.getElementById("myBody").innerHTML = JSON.stringify(xmlhttp.responseText);
+                createPosts(JSON.parse(xmlhttp.responseTest));
             } else if (xmlhttp.status == 400) {
                 alert("400 error");
             } else {
@@ -37,4 +36,4 @@ function loadJSON(url) {
     console.log('request sent');
 }
 
-loadJSON('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=team254');
+//loadJSON('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=team254');
