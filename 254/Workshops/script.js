c45b960c1e9ac2bf19930f85383d0063349213e7
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2017-08-10 22:04:07 -0700

    Animation for the menu

--- 254/Workshops/script.js
+++ 254/Workshops/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     $('.nav').hide();
+    $('.x').hide();
 
     $('.hamburger').click(function() {
-        $('.nav').toggle();
+        $('.hamburger').hide();    
+        $('.social').css('margin-right', '39px');        
+        $('.nav').slideDown("fast", function(){
+            $('.x').show();            
+        });
+    });
+
+    $('.x').click(function() {
+        $('.x').hide();        
+        $('.nav').slideUp("fast", function(){
+            $('.social').css('margin-right', '200px');            
+            $('.hamburger').show();            
+        });
     });
 });
